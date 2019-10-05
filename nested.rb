f7b5dfadcba0e180ce0a3c86eb008717ffa29c76
Mubarak Kareem <nelsonsmf@gmail.com>
2019-10-05 02:50:52 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -91,7 +91,7 @@ def adding_matz
      
      programmer_hash[:yukihiro_matsumoto]= :known_for
      programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
-    programmer_hash[:yukihiro_matsumoto][:known_for][:languages]
+    programmer_hash[:yukihiro_matsumoto][:known_for][:languages] = 
    
    puts programmer_hash
       
