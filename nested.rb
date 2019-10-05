0956fe2be2bd8a9ba3d1fdfc50ea79d8a01da5e6
Mubarak Kareem <nelsonsmf@gmail.com>
2019-10-05 02:50:34 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -90,8 +90,8 @@ def adding_matz
      }
      
      programmer_hash[:yukihiro_matsumoto]= :known_for
-     programmer_hash[:yukihiro_matsumoto][:known_for] = "Object Orientation"
-     
+     programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+    programmer_hash[:yukihiro_matsumoto][:known_for][:languages]
    
    puts programmer_hash
       
