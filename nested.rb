66cea06ea99e84171e55e7d25f429b43afda091c
Mubarak Kareem <nelsonsmf@gmail.com>
2019-10-05 03:22:40 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -144,6 +144,6 @@ def adding_to_dennis
         }
      }
      
-      programmer_hash[:dennis_ritchie][:languages][1] = 
+      programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
 
 end
